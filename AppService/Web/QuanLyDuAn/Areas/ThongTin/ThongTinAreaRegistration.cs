30b0f17c7e0452b0318801c91ca46d82ea205954
Pham Cong Trien <lukaspham22@gmail.com>
2020-11-30 15:49:29 +0700

    add actions config

---
+++ AppService/Web/QuanLyDuAn/Areas/ThongTin/ThongTinAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace QuanLyDuAn.Areas.ThongTin
+{
+    public class ThongTinAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "ThongTin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "ThongTin_default",
+                "ThongTin/{controller}/{action}/{id}",
+                new { controller ="Home",action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
