079d0d81792f5bc2a29f879a5e670b49ff615c20
Pham Cong Trien <lukaspham22@gmail.com>
2020-11-30 15:49:29 +0700

    add actions config

---
+++ AppService/Web/QuanLyDuAn/Areas/ThongTin/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuanLyDuAn.Areas.ThongTin.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: ThongTin/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
