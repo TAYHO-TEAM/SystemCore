2ad508881230745345acca7b86cfa1e76b1d0e22
Pham Cong Trien <lukaspham22@gmail.com>
2020-12-16 10:59:59 +0700

    commit

---
+++ AppService/TayHo.ProjectManager/PrjectManager.CMD/ProjectManager.CMD.Domain/IService/IMediaService.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Services.Common.Media;
+using System.Threading.Tasks;
+
+namespace ProjectManager.CMD.Domain.IService
+{
+    public interface IMediaService
+    {
+        //Task<List<MediaResponse>> UploadFileAsync(IEnumerable<IFormFile> formFiles);
+        Task<int> SaveFile(IFormFileCollection files);
+        Task<FileResponse> FetechFiles(string subDirectory);
+    }
+}
