164a613da5fc0a4bdb24e19198e7354ea2b6cd51
Pham Cong Trien <lukaspham22@gmail.com>
2020-12-17 10:04:45 +0700

    udpate

---
+++ AppService/TayHo.ProjectManager/PrjectManager.Read/ProjectManager.Read.Api/ViewModels/PlanRegisterResponseViewModel.cs
@@ -0,0 +1,18 @@
+﻿using ProjectManager.Read.Api.ViewModels.BaseClasses;
+using System;
+
+namespace ProjectManager.Read.Api.ViewModels
+{
+    public class PlanRegisterResponseViewModel : BaseResponseViewModel
+    {
+        public string Code { get; set; }
+        public string Title { get; set; }
+        public int? ProjectId { get; set; }
+        public int? ContractorInfoId { get; set; }
+        public string Description { get; set; }
+        public DateTime? FromDate { get; set; }
+        public DateTime? ToDate { get; set; }
+        public DateTime? ExpectFromDate { get; set; }
+        public DateTime? ExpectToDate { get; set; }
+    }
+}
---
+++ AppService/TayHo.ProjectManager/PrjectManager.Read/ProjectManager.Read.Sql/DTOs/DTO/PlanRegisterDTO.cs
@@ -0,0 +1,18 @@
+﻿using ProjectManager.Read.Sql.DTOs.BaseClasses;
+using System;
+
+namespace ProjectManager.Read.Sql.DTOs.DTO
+{
+    public class PlanRegisterDTO : DTOBase
+    {
+        public string Code { get; set; }
+        public string Title { get; set; }
+        public int? ProjectId { get; set; }
+        public int? ContractorInfoId { get; set; }
+        public string Description { get; set; }
+        public DateTime? FromDate { get; set; }
+        public DateTime? ToDate { get; set; }
+        public DateTime? ExpectFromDate { get; set; }
+        public DateTime? ExpectToDate { get; set; }
+    }
+}
