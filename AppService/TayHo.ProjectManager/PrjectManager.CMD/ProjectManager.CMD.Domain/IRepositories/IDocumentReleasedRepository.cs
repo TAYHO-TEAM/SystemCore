5633edd092d089af9e7bfbea6e77ff958fc9bb81
Pham Cong Trien <lukaspham22@gmail.com>
2020-12-23 18:23:12 +0700

    update phat hanh

--- AppService/TayHo.ProjectManager/PrjectManager.CMD/ProjectManager.CMD.Domain/IRepositories/IDocumentReleasedRepository.cs
+++ AppService/TayHo.ProjectManager/PrjectManager.CMD/ProjectManager.CMD.Domain/IRepositories/IDocumentReleasedRepository.cs
@@ -1,9 +1,11 @@
 ﻿using ProjectManager.CMD.Domain.DomainObjects;
 using Services.Common.DomainObjects.Interfaces;
+using System.Threading.Tasks;
 
 namespace ProjectManager.CMD.Domain.IRepositories
 {
     public interface IDocumentReleasedRepository : ICmdRepository<DocumentReleased>
     {
+        Task<bool> IsCreatedDocumentReleasedAsync(int DocumentTypeId, int AccountId, int Id);
     }
 }
--- AppService/TayHo.ProjectManager/PrjectManager.CMD/ProjectManager.CMD.Infrastructure/Repositories/DocumentReleasedRepository.cs
+++ AppService/TayHo.ProjectManager/PrjectManager.CMD/ProjectManager.CMD.Infrastructure/Repositories/DocumentReleasedRepository.cs
@@ -1,7 +1,10 @@
-﻿using ProjectManager.CMD.Domain.DomainObjects;
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using ProjectManager.CMD.Domain.DomainObjects;
 using ProjectManager.CMD.Domain.IRepositories;
 using Services.Common.APIs.Cmd.EF;
-
+using System.Data;
+using System.Threading.Tasks;
 
 namespace ProjectManager.CMD.Infrastructure.Repositories
 {
@@ -9,6 +12,19 @@ namespace ProjectManager.CMD.Infrastructure.Repositories
     {
         public DocumentReleasedRepository(QuanLyDuAnContext dbContext) : base(dbContext)
         {
+        }
+        public async Task<bool> IsCreatedDocumentReleasedAsync(int DocumentTypeId, int AccountId, int Id)
+        {
+            await using var cmd = _dbContext.Database.GetDbConnection().CreateCommand();
+            await cmd.Connection.OpenAsync();
+            cmd.CommandText = "sp_DocumentReleased_CreateByAccountID";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add(new SqlParameter("@DocumentTypeId", DocumentTypeId));
+            cmd.Parameters.Add(new SqlParameter("@AccountId", AccountId));
+            cmd.Parameters.Add(new SqlParameter("@DocumentReleasedId", Id));
+            var result = await cmd.ExecuteNonQueryAsync().ConfigureAwait(false);
+            await cmd.Connection.CloseAsync();
+            return (bool)(result == 0 ? false : true);
         }
     }
 }
