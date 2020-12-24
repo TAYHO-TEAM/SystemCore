ef72e559340d97f8382f4bdbe50a1a1b0e0c7837
Pham Cong Trien <lukaspham22@gmail.com>
2020-12-24 11:45:31 +0700

    update add user

---
+++ AppService/TayHo.ProjectManager/PrjectManager.Read/ProjectManager.Read.Api/ViewModels/AccountInfoResponseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ProjectManager.Read.Api.ViewModels.BaseClasses;
+
+namespace ProjectManager.Read.Api.ViewModels
+{
+    public class AccountInfoResponseViewModel //: BaseResponseViewModel
+    {
+        public int? AccountId { get; set; }
+        public string AccountName { get; set; }
+        public string UserName { get; set; }
+        public byte[]? AvatarImg { get; set; }
+        public string Title { get; set; }
+        public string Department { get; set; }
+    }
+}
---
+++ AppService/TayHo.ProjectManager/PrjectManager.Read/ProjectManager.Read.Sql/DTOs/DTO/AccountInfoDTO.cs
@@ -0,0 +1,14 @@
+﻿using ProjectManager.Read.Sql.DTOs.BaseClasses;
+
+namespace ProjectManager.Read.Sql.DTOs.DTO
+{
+    public class AccountInfoDTO 
+    {
+        public int? AccountId { get; set; }
+        public string AccountName { get; set; }
+        public string UserName { get; set; }
+        public byte[]? AvatarImg { get; set; }
+        public string Title { get; set; }
+        public string Department { get; set; }
+    }
+}
