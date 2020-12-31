ea75f18f37bca6c48180af18053abc40f4a94452
Pham Cong Trien <lukaspham22@gmail.com>
2020-12-31 15:29:56 +0700

    update

--- AppService/Web/QuanLyDuAn/Scripts/page/thongtin.js
+++ AppService/Web/QuanLyDuAn/Scripts/page/thongtin.js
@@ -11,12 +11,14 @@ const ACTION_READ_REPLY = "/ResponseRegistReply";
 const ACTION_READ_ACCOUNTINFO = "/ACCOUNTINFO";
 
 const ACTION_CMD_REQUESTREGIST = "/RequestRegist";
+const ACTION_CMD_RESPONSEREGIST = "/ResponseRegist";
 
 
 let customStore_CMD_READ_WITHPROJECTID = (CMD,READ)=> new DevExpress.data.CustomStore({
     key: "id",
     load: (values) => {
         let deferred = $.Deferred(), params = { 'FindId': 'projectId,' + PROJECTID };
+       
         if (values.filter && values.filter[0] == "parentId") params['FindParentId'] = values.filter[2];
         if (values.sort) {
             params['SortCol'] = values.sort[0].selector;
@@ -44,9 +46,45 @@ let customStore_CMD_READ_WITHPROJECTID = (CMD,READ)=> new DevExpress.data.Custom
     update: (key, values) => ajax_update(URL_API_PM_CMD + CMD, key, values),
     remove: (key) => ajax_delete(URL_API_PM_CMD + CMD, key),
 });
-
+let customStore_UPDATE_READ = (ID,CMD,READ) => new DevExpress.data.CustomStore({
+    key: "id",
+    load: (values) => {
+        var deferred = $.Deferred();
+        var params = {
+            'PageSize': isNullOrEmpty(values.take) ? values.take : 0,
+            'PageNumber': (isNullOrEmpty(values.take) && isNullOrEmpty(values.skip)) ? ((values.skip / values.take) + 1) : 0,
+            'FindId': ID
+        }
+        if (values.sort) {
+            params['SortCol'] = values.sort[0].selector;
+            params['SortADSC'] = values.sort[0].desc;
+        }
+        $.ajax({
+            headers: header, dataType: "json",
+            data: params,
+            url: URL_API_PM_READ + READ,
+            success: function (data) {
+                var list = data.result.items;
+                deferred.resolve(
+                    list,
+                    {
+                        totalCount: list.length,
+                    }
+                );
+            },
+            error: function (xhr, textStatus, errorThrown) {
+                console.log(xhr.responseJSON);
+                deferred.reject("Có lỗi xảy ra trong quá trình lấy danh sách 'Hạng mục'. Mở Console để xem chi tiết.");
+            },
+            timeout: 10000
+        });
+        return deferred.promise();
+    },
+    update: (key, values) => ajax_update(URL_API_PM_CMD + CMD, key, values),
+});
 let customStore_READ_ALL_ACC = (READ) =>new DevExpress.data.CustomStore({
-    key: "id", loadMode: "raw",
+    key: "id",
+    loadMode: "raw",
     load: (values) => {
         var deferred = $.Deferred();
         $.ajax({
