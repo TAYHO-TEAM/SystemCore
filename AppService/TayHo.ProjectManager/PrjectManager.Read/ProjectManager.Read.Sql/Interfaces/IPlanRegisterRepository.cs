d23f6d3e20071d87898b9c3ccc3e74e8970c779a
Pham Cong Trien <lukaspham22@gmail.com>
2021-01-06 18:10:03 +0700

    ádasd

---
+++ AppService/TayHo.ProjectManager/PrjectManager.Read/ProjectManager.Read.Sql/Interfaces/IPlanRegisterRepository.cs
@@ -0,0 +1,14 @@
+﻿using ProjectManager.Read.Sql.DTOs.DTO;
+using ProjectManager.Read.Sql.Parameters;
+using Services.Common.Paging;
+using System.Threading.Tasks;
+
+namespace ProjectManager.Read.Sql.Interfaces
+{
+    public interface IPlanRegisterRepository<T> where T : class
+    {
+        //Task<PagingItems<T>> GetWithPaggingAsync(RequestBaseFilterParam requetsBaseFilterParam);GetWithDetailAsync
+        Task<PagingItems<T>> GetWithPaggingContractorInfoAsync(RequestHasAccountIdFilterParam requetsBaseFilterParam);
+    }
+}
+
