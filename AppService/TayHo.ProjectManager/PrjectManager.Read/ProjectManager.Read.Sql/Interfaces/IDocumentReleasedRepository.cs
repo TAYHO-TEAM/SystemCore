6829f213e6e4f9447138a99bf8d25417c4fb0c7c
Pham Cong Trien <lukaspham22@gmail.com>
2021-01-06 11:25:45 +0700

    udpate

---
+++ AppService/TayHo.ProjectManager/PrjectManager.Read/ProjectManager.Read.Sql/Interfaces/IDocumentReleasedRepository.cs
@@ -0,0 +1,14 @@
+﻿using ProjectManager.Read.Sql.DTOs.DTO;
+using ProjectManager.Read.Sql.Parameters;
+using Services.Common.Paging;
+using System.Threading.Tasks;
+
+namespace ProjectManager.Read.Sql.Interfaces
+{
+    public interface IDocumentReleasedRepository<T> where T : class
+    {
+        Task<PagingItems<T>> GetWithPaggingStepPermistionAsync(RequestHasAccountIdFilterParam requetsBaseFilterParam);
+        Task<PagingItems<T>> GetWithPaggingResiveIdAsync(RequestHasAccountIdFilterParam requetsBaseFilterParam);
+    }
+}
+
