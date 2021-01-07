a9d1975e9a872d4a0581306513908d23f1b4a70b
Pham Cong Trien <lukaspham22@gmail.com>
2021-01-07 13:21:49 +0700

    ấd

---
+++ AppService/TayHo.ProjectManager/PrjectManager.Read/ProjectManager.Read.Sql/Interfaces/IDocumentReleasedLogRepository.cs
@@ -0,0 +1,13 @@
+﻿using ProjectManager.Read.Sql.DTOs.DTO;
+using ProjectManager.Read.Sql.Parameters;
+using Services.Common.Paging;
+using System.Threading.Tasks;
+
+namespace ProjectManager.Read.Sql.Interfaces
+{
+    public interface IDocumentReleasedLogRepository<T> where T : class
+    {
+        Task<PagingItems<T>> GetWithPaggingDetailAsync(RequestHasAccountIdFilterParam requetsBaseFilterParam);
+    }
+}
+
---
+++ AppService/TayHo.ProjectManager/PrjectManager.Read/ProjectManager.Read.Sql/Repositories/DocumentReleasedLogRepository.cs
@@ -0,0 +1,44 @@
+﻿using Dapper;
+using Dapper.Common;
+using ProjectManager.Read.Sql.DTOs.DTO;
+using ProjectManager.Read.Sql.Interfaces;
+using ProjectManager.Read.Sql.Parameters;
+using Services.Common.Paging;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectManager.Read.Sql.Repositories
+{
+    public class DocumentReleasedLogRepository<T> : IDocumentReleasedLogRepository<T> where T : class
+    {
+        protected readonly ISqlConnectionFactory _connectionFactory;
+
+        public DocumentReleasedLogRepository(ISqlConnectionFactory connectionFactory)
+        {
+            _connectionFactory = connectionFactory ?? throw new ArgumentNullException(nameof(connectionFactory));
+        }
+        public async Task<PagingItems<T>> GetWithPaggingDetailAsync(RequestHasAccountIdFilterParam requestBaseFilterParam)
+        {
+            requestBaseFilterParam.ColumName = requestBaseFilterParam.ColumName?? "*" ;
+            var result = new PagingItems<T>
+            {
+                PagingInfo = new PagingInfo
+                {
+                    PageNumber = requestBaseFilterParam.PageNumber,
+                    PageSize = requestBaseFilterParam.PageSize
+                }
+            };
+            using var conn = await _connectionFactory.CreateConnectionAsync();
+            using var rs = conn.QueryMultipleAsync("sp_DocumentReleasedLog_GetListDetail", requestBaseFilterParam, commandType: CommandType.StoredProcedure).Result;
+            result.PagingInfo.TotalItems = await rs.ReadSingleAsync<int>().ConfigureAwait(false);
+            result.Items = await rs.ReadAsync<T>().ConfigureAwait(false);
+
+            return result;
+        }
+      
+    }
+}
