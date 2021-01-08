aef2766e55aab86313645ebe8c63eb1cb771efaf
dangtm77 <dangtm77@gmail.com>
2021-01-08 18:01:04 +0700

    dangtm

---
+++ AppService/Web/QuanLyDuAn/Areas/NganSach/Controllers/NghiemThuController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuanLyDuAn.Areas.NganSach.Controllers
+{
+    public class NghiemThuController : Controller
+    {
+        // GET: NganSach/NghiemThu
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
