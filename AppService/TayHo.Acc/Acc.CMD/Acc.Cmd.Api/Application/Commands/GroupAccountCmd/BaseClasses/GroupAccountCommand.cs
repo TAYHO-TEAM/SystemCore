6dac863386f73a5919d59a69601deeb2a1959a64
Pham Cong Trien <lukaspham22@gmail.com>
2021-01-25 17:29:37 +0700

    'update'

--- AppService/TayHo.Acc/Acc.CMD/Acc.Cmd.Api/Application/Commands/GroupAccountCmd/BaseClasses/GroupAccountCommand.cs
+++ AppService/TayHo.Acc/Acc.CMD/Acc.Cmd.Api/Application/Commands/GroupAccountCmd/BaseClasses/GroupAccountCommand.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Acc.Cmd.Api.Application.Commands
 {
     public class GroupAccountCommandResponseDTO : GroupAccountCommandSet
     {
      
+    }
+    public class GroupAccountListCommandResponseDTO 
+    {
+        public List<GroupAccountCommandResponseDTO> groupAccountCommandResponseDTOs { get; set; } = new List<GroupAccountCommandResponseDTO>();
     }
 }
