5c6a4562fabe3572b0f737e31bde4c236e4648e8
Pham Cong Trien <lukaspham22@gmail.com>
2021-02-02 17:52:39 +0700

    comiit

--- AppService/TayHo.ProjectManager/PrjectManager.Read/ProjectManager.Read.Api/ViewModels/PlanRegisterResponseViewModel.cs
+++ AppService/TayHo.ProjectManager/PrjectManager.Read/ProjectManager.Read.Api/ViewModels/PlanRegisterResponseViewModel.cs
@@ -18,4 +18,19 @@ namespace ProjectManager.Read.Api.ViewModels
         public DateTime? ExpectRequestDate { get; set; }
         public DateTime? ExpectResponseDate { get; set; }
     }
+    public class PlanRegisterAccountInfoResponseViewModel : BaseResponseAccountInfoViewModel
+    {
+        public int? ParentId { get; set; }
+        public int? DocumentTypeId { get; set; }
+        public int? WorkItemId { get; set; }
+        public string Code { get; set; }
+        public string Title { get; set; }
+        public int? ProjectId { get; set; }
+        public int? ContractorInfoId { get; set; }
+        public string Description { get; set; }
+        public DateTime? RequestDate { get; set; }
+        public DateTime? ResponseDate { get; set; }
+        public DateTime? ExpectRequestDate { get; set; }
+        public DateTime? ExpectResponseDate { get; set; }
+    }
 }
