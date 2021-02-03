4832765aeaa17459348c25ea69c1c07efcd926bb
Pham Cong Trien <lukaspham22@gmail.com>
2021-02-03 10:00:06 +0700

    update

--- AppService/TayHo.ProjectManager/PrjectManager.Read/ProjectManager.Read.Sql/DTOs/DTO/NotifyDTO.cs
+++ AppService/TayHo.ProjectManager/PrjectManager.Read/ProjectManager.Read.Sql/DTOs/DTO/NotifyDTO.cs
@@ -9,7 +9,7 @@ namespace ProjectManager.Read.Sql.DTOs.DTO
         public string Category { get; set; }
         public string Message { get; set; }
         public string Link { get; set; }
-        public int? NotifyTemplateId { get; set; }
+        public int? TemplateId { get; set; }
         public string Title { get; set; }
         public string Sub { get; set; }
         public string BodyContent { get; set; }
