8176e1791482bcdb2c7b348b53caa0567347cf45
Pham Cong Trien <lukaspham22@gmail.com>
2021-02-08 17:06:56 +0700

    update appointment

--- AppService/TayHo.ProjectManager/PrjectManager.CMD/ProjectManager.CMD.Infrastructure/Service/SendMailService.cs
+++ AppService/TayHo.ProjectManager/PrjectManager.CMD/ProjectManager.CMD.Infrastructure/Service/SendMailService.cs
@@ -22,8 +22,7 @@ namespace ProjectManager.CMD.Infrastructure.Service
         private readonly IWebHostEnvironment _env;
         private readonly IHttpClientFactory _clientFactory;
         private readonly ProfileMailOptions _profileMailOptions;
-        private readonly QuanLyDuAnContext dbContext;
-        public SendMailService(IOptionsSnapshot<ProfileMailOptions> snapshotOptionsAccessor, IHttpClientFactory clientFactory, IWebHostEnvironment env) 
+        public SendMailService(IOptionsSnapshot<ProfileMailOptions> snapshotOptionsAccessor, IHttpClientFactory clientFactory, IWebHostEnvironment env)
         {
             _profileMailOptions = snapshotOptionsAccessor.Value;
             _clientFactory = clientFactory;
@@ -43,8 +42,8 @@ namespace ProjectManager.CMD.Infrastructure.Service
             str.AppendLine(string.Format("DTEND:{0:yyyyMMddTHHmmss}", EndDate));
             str.AppendLine("LOCATION: " + LocationTaget);
             str.AppendLine(string.Format("UID:{0}", Guid.NewGuid()));
-            str.AppendLine(string.Format("DESCRIPTION:{0}", Body));
-            str.AppendLine(string.Format("X-ALT-DESC;FMTTYPE=text/html:{0}", Body));
+            str.AppendLine(string.Format("DESCRIPTION:{0}", Body.Replace("\n", "<br>")));
+            str.AppendLine(string.Format("X-ALT-DESC;FMTTYPE=text/html:{0}", Body.Replace("\n", "<br>")));
             str.AppendLine(string.Format("SUMMARY:{0}", Subject));
             str.AppendLine(string.Format("ORGANIZER;CN=\"{0}\":MAILTO:{1}", _profileMailOptions.name, _profileMailOptions.name));
             str.AppendLine(string.Format("ATTENDEE;CN=\"{0}\";RSVP=TRUE:mailto:{1}", string.Join(",", MailTo), string.Join(",", MailTo)));
@@ -65,21 +64,32 @@ namespace ProjectManager.CMD.Infrastructure.Service
 
             MailMessage msg = new MailMessage();///MailFrom, string.Join(",", MailTo)
             msg.Attachments.Add(attach);
-            msg.From = new MailAddress(MailFrom, "Hệ thông");
-            foreach (string tomail in MailTo)
+            msg.From = new MailAddress(_profileMailOptions.userName, _profileMailOptions.name);
+            if (MailTo != null && MailTo.Count > 0)
             {
-                msg.To.Add(tomail);
+                foreach (string tomail in MailTo)
+                {
+                    msg.To.Add(tomail);
+                }
             }
-            foreach (string ccmail in MailCC)
+            if (MailCC != null && MailCC.Count > 0)
             {
-                msg.CC.Add(ccmail);
+                foreach (string ccmail in MailCC)
+                {
+                    msg.CC.Add(ccmail);
+                }
             }
-            foreach (string bccmail in MailBCC)
+            if (MailBCC != null && MailBCC.Count > 0)
             {
-                msg.Bcc.Add(bccmail);
-            }
+                foreach (string bccmail in MailBCC)
+                {
+                    msg.Bcc.Add(bccmail);
+                }
+            } 
             ContentType contype = new ContentType("text/calendar");
             contype.Parameters.Add("method", "REQUEST");
+
+  
             //  contype.Parameters.Add("name", "Meeting.ics");
             AlternateView avCal = AlternateView.CreateAlternateViewFromString(str.ToString(), contype);
             msg.AlternateViews.Add(avCal);
@@ -92,6 +102,39 @@ namespace ProjectManager.CMD.Infrastructure.Service
             smtpclient.Credentials = new NetworkCredential(_profileMailOptions.userName, _profileMailOptions.passWord);
             smtpclient.DeliveryMethod = SmtpDeliveryMethod.Network;
             smtpclient.Send(msg);
+        }
+
+        private static string MeetingRequestString(string from, List<string> toUsers, string subject, string desc, string location, DateTime startTime, DateTime endTime, int? eventID = null, bool isCancel = false)
+        {
+            StringBuilder str = new StringBuilder();
+
+            str.AppendLine("BEGIN:VCALENDAR");
+            str.AppendLine("PRODID:-//Microsoft Corporation//Outlook 12.0 MIMEDIR//EN");
+            str.AppendLine("VERSION:2.0");
+            str.AppendLine(string.Format("METHOD:{0}", (isCancel ? "CANCEL" : "REQUEST")));
+            str.AppendLine("BEGIN:VEVENT");
+
+            str.AppendLine(string.Format("DTSTART:{0:yyyyMMddTHHmmssZ}", startTime.ToUniversalTime()));
+            str.AppendLine(string.Format("DTSTAMP:{0:yyyyMMddTHHmmss}", DateTime.Now));
+            str.AppendLine(string.Format("DTEND:{0:yyyyMMddTHHmmssZ}", endTime.ToUniversalTime()));
+            str.AppendLine(string.Format("LOCATION: {0}", location));
+            str.AppendLine(string.Format("UID:{0}", (eventID.HasValue ? "Meeting" + eventID : Guid.NewGuid().ToString())));
+            str.AppendLine(string.Format("DESCRIPTION:{0}", desc.Replace("\n", "<br>")));
+            str.AppendLine(string.Format("X-ALT-DESC;FMTTYPE=text/html:{0}", desc.Replace("\n", "<br>")));
+            str.AppendLine(string.Format("SUMMARY:{0}", subject));
+
+            str.AppendLine(string.Format("ORGANIZER;CN=\"{0}\":MAILTO:{1}", from, from));
+            str.AppendLine(string.Format("ATTENDEE;CN=\"{0}\";RSVP=TRUE:mailto:{1}", string.Join(",", toUsers), string.Join(",", toUsers)));
+
+            str.AppendLine("BEGIN:VALARM");
+            str.AppendLine("TRIGGER:-PT15M");
+            str.AppendLine("ACTION:DISPLAY");
+            str.AppendLine("DESCRIPTION:Reminder");
+            str.AppendLine("END:VALARM");
+            str.AppendLine("END:VEVENT");
+            str.AppendLine("END:VCALENDAR");
+
+            return str.ToString();
         }
     }
 }
