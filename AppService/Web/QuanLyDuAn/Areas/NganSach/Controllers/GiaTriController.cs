22750785cfa6c621b6a4f2943d785536e8daf33c
dangtm77 <dangtm77@gmail.com>
2021-03-04 16:49:31 +0700

    dangtm

---
+++ AppService/Web/QuanLyDuAn/Areas/NganSach/Controllers/GiaTriController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuanLyDuAn.Areas.NganSach.Controllers
+{
+    public class GiaTriController : Controller
+    {
+        // GET: NganSach/GiaTri
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
