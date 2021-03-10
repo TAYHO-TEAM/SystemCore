8c2d19547ccc2fcf96457305dbc840d79039b57a
dangtm77 <dangtm77@gmail.com>
2021-03-10 17:28:54 +0700

    dangtm

---
+++ AppService/Web/QuanLyDuAn/Scripts/page/hethong.js
@@ -0,0 +1,8 @@
+﻿const ACTION_ACCOUNT = "/Accounts"; 
+const ACTION_ACCOUNT_INFO = "/AccountInfo"; 
+
+var listActiveStatus = [
+    { value: true, text: "Hoạt động", color: "success", icon: 'fa fa-check-circle' },
+    { value: false, text: "Tạm dừng", color: "danger", icon: 'fa fa-minus-circle' },
+]
+ 
