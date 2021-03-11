aa7d0b9636920c0db387d5e2ad927ce9c74cb680
dangtm77 <dangtm77@gmail.com>
2021-03-11 18:12:47 +0700

    dangtm

--- AppService/Web/QuanLyDuAn/Scripts/page/hethong.js
+++ AppService/Web/QuanLyDuAn/Scripts/page/hethong.js
@@ -1,5 +1,10 @@
 ﻿const ACTION_ACCOUNT = "/Accounts"; 
 const ACTION_ACCOUNT_INFO = "/AccountInfo"; 
+const ACTION_GROUP_ACCOUNT = "/GroupAccount"; 
+const ACTION_GROUP = "/Groups"; 
+const ACTION_GROUP_ACTION_PERMISTION = "/GroupActionPermistion"; 
+const ACTION_PERMISTION = "/Permistions"; 
+const ACTION_ACTION = "/Actions"; 
 
 var listActiveStatus = [
     { value: true, text: "Hoạt động", color: "success", icon: 'fa fa-check-circle' },
